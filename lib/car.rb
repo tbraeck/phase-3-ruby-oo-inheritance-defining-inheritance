f31286dd89c9465394d064b45384aaf3884823b5
Tate <tatebraeckel@gmail.com>
2023-04-03 13:44:25 -0600

    commit tree 941d7056276ca9f70752e40260cfd2238359a04c

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,11 @@
+
+class Car < Vehicle 
+
+def go 
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+end
+end
+
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
